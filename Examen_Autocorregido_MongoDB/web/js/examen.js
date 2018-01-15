85b22b80580814be94d5f0bdf23e12d9d28103e3
Andreu <Andreu@DESKTOP-9G33HKJ>
2018-01-15 23:03:00 +0100

    - Eliminadas clases innecesarias - Creadas clases necesarias - Redireccion con session storage - Esqueleto de generador de exámen

---
+++ Examen_Autocorregido_MongoDB/web/js/examen.js
@@ -0,0 +1,102 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var DNI = null;
+var tipoExamen = null;
+var nota = null;
+
+$(document).ready(function () {
+    comprobarDatos();
+    generarExamen();
+    
+    //Clicks
+    $("#myForm").submit(function (){
+        corregirExamen();
+        return false;
+    });
+    
+    
+});
+
+function comprobarDatos() {
+    DNI = sessionStorage.getItem("_DNI");
+    tipoExamen = sessionStorage.getItem("_tipoExamen");
+
+    //Si no existen los datos, redireccionar al index
+    if (DNI === null || tipoExamen === null) {
+        location.replace("./index.html");
+    }
+
+    //Si existen los datos, borrarlos para la proxima vez
+    sessionStorage.removeItem("_DNI");
+    sessionStorage.removeItem("_tipoExamen");
+}
+
+function generarExamen() {
+    var url = "getExamenServlet";
+    $.ajax({
+        method: "POST",
+        url: url,
+        data: {tipoExamen: tipoExamen},
+        success: function (rsp) {
+            showToast("Exámen " + (parseInt(tipoExamen) + 1), "Cargado correctamente", "success", "#36B62D");
+            // AQUI SE GENERARA:
+            //    ....
+            //    ....
+            //    ....
+        },
+        error: function (e) {
+            if (e["responseJSON"] === undefined) {
+                showToast("ERROR DESCONOCIDO", "Inténtelo más tarde", "error", "#D43721");
+            } else {
+                showToast(e["responseJSON"]["error"], "", "error", "#D43721");
+            }
+        }
+    });
+}
+
+function corregirExamen() {
+    //   ....
+    //   ....
+    //   ....
+    
+    //Al final guardamos la nota
+    guardarNota();
+}
+
+function guardarNota(){
+    var url = "guardarNotaServlet";
+    $.ajax({
+        method: "POST",
+        url: url,
+        data: {DNI: DNI, tipoExamen: tipoExamen, nota:nota},
+        success: function (rsp) {
+            showToast("NOTA GUARDADA", "Nota: "+nota, "success", "#36B62D");
+        },
+        error: function (e) {
+            if (e["responseJSON"] === undefined) {
+                showToast("ERROR DESCONOCIDO", "Inténtelo más tarde", "error", "#D43721");
+            } else {
+                showToast(e["responseJSON"]["error"], "", "error", "#D43721");
+            }
+        }
+    });
+}
+
+function showToast(head, text, icon, bgColor) {
+    $.toast({
+        text: text, // Text that is to be shown in the toast
+        heading: head, // Optional heading to be shown on the toast
+        icon: icon, // Type of toast icon: warning | success | error | info
+        showHideTransition: 'fade', // fade, slide or plain
+        allowToastClose: false, // Boolean value true or false
+        hideAfter: 3000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
+        position: 'top-center', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
+        textAlign: 'left', // Text alignment i.e. left, right or center
+        loader: true, // Whether to show loader or not. True by default
+        loaderBg: '#9EC600', // Background color of the toast loader
+        bgColor: bgColor
+    });
+}
--- Examen_Autocorregido_MongoDB/web/js/index.js
+++ Examen_Autocorregido_MongoDB/web/js/index.js
@@ -16,28 +16,11 @@ $(document).ready(function () {
     $("#btnConfirmarTest").click(function () {
         var examen = $("#selectExamenes")[0].selectedIndex;
         var DNI = $("#inpDNI").val();
-        var url = "realizarExamenServlet"; //Servlet
-        // Examen vale 0 / 1 / 2
 
-        $.ajax({
-            method: "POST",
-            url: url,
-            data: {DNI:DNI, examen: examen},
-            success: function () {
-                //El servelt se encarga de redireccionar 
-                //¿Al redireccionar.. carga el jsp o peticion ajax para devolver
-                //las preguntas?
-                //Enviamos el dni como atributo y vemos que tenga un dni o redireccionames
-                //Y cargamos con peticion el examen
-            },
-            error: function (e) {
-                if (e["responseJSON"] === undefined) {
-                    showToast("ERROR DESCONOCIDO", "Inténtelo más tarde", "error", "#D43721");
-                } else {
-                    showToast(e["responseJSON"]["error"], "", "error", "#D43721");
-                }
-            }
-        });
+        sessionStorage.setItem("_DNI", DNI);
+        sessionStorage.setItem("_tipoExamen", examen);
+
+        location.replace("./examen.html");
     });
 });
 
